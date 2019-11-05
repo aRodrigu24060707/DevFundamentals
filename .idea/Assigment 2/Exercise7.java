67293541b912751accbdeebb19fbd986e361c867
arodrigu <angelica.rodriguez@jalasoft.com>
2019-11-04 21:51:43 -0400

    Task 2

---
+++ .idea/Assigment 2/Exercise7.java
@@ -0,0 +1,29 @@
+/**
+ * Checking whether two arrays contain a mismatch
+ */
+public class Exercise7 {
+    public static void main(String[] args) {
+        int[] firstTestArrayOne = {1, 2, 3, 4, 5, 6};
+        int[] firstTestArrayTwo = {1, 2, 3, 4, 5, 6};
+
+        int resultFirstTest = mismatch(firstTestArrayOne, firstTestArrayTwo);
+        System.out.println(resultFirstTest); //result should be -1
+
+
+        int[] secondTestArrayOne = {1, 2, 3, 4, 5, 6};
+        int[] secondTestArrayTwo = {1, 2, 3, 4, 2, 6};
+
+        int resultSecondTest = mismatch(secondTestArrayOne, secondTestArrayTwo);
+        System.out.println(resultSecondTest); //result should be 4
+    }
+
+    public static int mismatch(int[] first, int[] second) {
+       int result = -1;
+       for(int index = 0; index < first.length; index++) {
+           if (first[index]!=second[index]) {
+               result = 4;
+           }
+       }
+       return result;
+    }
+}
